bdd010719727264b356ce694afa076f52870c370
imoto-y <imoto-y@sys.i.kyoto-u.ac.jp>
2020-11-21 23:12:28 +0900

    libtorchの設定をプロジェクトに追加

--- source/learn/learner.cpp
+++ source/learn/learner.cpp
@@ -2180,6 +2180,32 @@ bool LearnerThink::save(bool is_final)
 	return false;
 }
 
+#if defined(USE_LIBTORCH)
+//! LearnerThinkのlibtorch版
+struct LearnerThinkTorch : public MultiThink {
+	LearnerThinkTorch(SfenReader& sr_) : sr(sr_), stop_flag(false), save_only_once(false) {
+		// 
+	}
+
+	virtual void thread_worker(size_t thread_id);
+
+	// sfenの読み出し器
+	SfenReader& sr;
+
+	bool stop_flag;
+
+	// 評価関数の保存するときに都度フォルダを掘るかのフラグ。
+	// trueだとフォルダを掘らない。
+	bool save_only_once;
+
+	TaskDispatcher task_dispatcher;
+};
+
+void LearnerThinkTorch::thread_worker(size_t thread_id) {
+
+}
+#endif // defined(USE_LIBTORCH)
+
 // shuffle_files() , shuffle_files_quick()の下請けで、書き出し部分。
 // output_file_name : 書き出すファイル名
 // prng : 乱数
